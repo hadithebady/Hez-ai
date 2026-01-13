b866f346a70564044628d485552a42442cc09d29
hadithebady <mazm33n@gmail.com>
2026-01-13 18:25:57 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
-console.log("RUNNING CORRECT SERVER.JS");
-
 const express = require("express");
 const path = require("path");
 
 const app = express();
-
 app.use(express.json());
 app.use(express.static(__dirname));
 
+let pluginConnected = false;
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// 🔑 PLUGIN CONNECT ENDPOINT
+app.post("/plugin/connect", (req, res) => {
+  pluginConnected = true;
+  console.log("Plugin connected");
+  res.json({ success: true, message: "Connected successfully" });
+});
+
+// 🔒 CHECK CONNECTION
+app.get("/plugin/status", (req, res) => {
+  res.json({ connected: pluginConnected });
 });
 
 const PORT = process.env.PORT || 3000;
